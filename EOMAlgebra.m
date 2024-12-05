bdc1b2a73140014786b472e2d2bdd855b3bf9001
nicholashirsch <nicholashirsch@ufl.edu>
2024-12-04 23:49:59 -0500

    Fixes to EOM algerba

--- EOMAlgebra.m
+++ EOMAlgebra.m
@@ -29,8 +29,6 @@ clear; clc;
 %             theta, phi, psi: Euler angles to go from I to V.
 %    thetaDot, phiDot, psiDot: Angular velocities of Euler angles in I.
 % thetaDDot, phiDDot, psiDDot: Angular accelerations of Euler angles in I.
-%                     p, q, r: Angular velocities of vehicle in V.
-%            pDot, qDot, rDot: Angular accelerations of vehicle in V.
 %                           T: Thrust.
 %                          xi: First gimbal axis.
 %                        zeta: Second gimbal axis.
@@ -47,12 +45,13 @@ syms x y z xDot yDot zDot xDDot yDDot zDDot ...
 %    T0 xi0 zeta0 tauR0
 
 
+
+
 % Define rotation matricies used in shifting all terms in the EOM to V.
 Tu2t = euler2rMatrix(zeta, 2)*euler2rMatrix(xi, 3); % Thrust to vehicle.
 Tt2u = Tu2t.';
 
-Ti2u = euler2rMatrix(phi, 1)*euler2rMatrix(theta, 2)*...
-    euler2rMatrix(psi, 3); % Vehicle to inertial.
+Ti2u = euler2rMatrix(psi, 3)*euler2rMatrix(theta, 2)*euler2rMatrix(phi, 1); % Vehicle to inertial.
 Tu2i = Ti2u.';
 
 
@@ -80,34 +79,22 @@ v    = [xDot; yDot; zDot];    % Frame: I.
 a    = [xDDot; yDDot; zDDot]; % Frame: I.
 
 % STEP 2: KINEMATICS - ANGULAR.
-% Note: p, q, r, pDot, qDot, and rDot are intermediate variables which may be
-%       defined in terms of the Euler angles and their derivatives.
-p    = phiDot - psiDot*sin(theta);
-q    = thetaDot*cos(phi) + psiDot*sin(phi)*cos(theta);
-r    = psiDot*cos(phi)*cos(theta) - thetaDot*sin(phi);
-pDot = phiDDot - psiDDot*sin(theta) - psiDot*cos(theta)*thetaDot;
-qDot = -thetaDot*sin(phi)*phiDot + thetaDDot*cos(phi) + psiDDot*sin(phi)*cos(theta) ...
-        + psiDot*cos(phi)*phiDot*cos(theta) - psiDot*sin(phi)*sin(theta)*thetaDot;
-rDot = psiDDot*cos(phi)*cos(theta) - psiDot*sin(phi)*phiDot*cos(theta) ...
-        - psiDot*cos(phi)*sin(theta)*thetaDot - thetaDDot*sin(phi) ...
-        - thetaDot*cos(phi)*phiDot;
-
-omega = [p; q; r];                    % Frames: I -> U.
+omega = [phiDot; thetaDot; psiDot];   % Frames: I -> U.
+omegaDot = [phiDDot; thetaDDot; psiDDot]; % Frames: I -> U.
 I     = [Ixx 0 0; 0 Iyy 0; 0 0 Izz];  % Frame:  V.
 H     = I*omega;                      % Frame:  I.
-HDot  = [Ixx*pDot + p*q*r*(Izz-Iyy);  
-         Iyy*qDot + p*q*r*(Izz-Ixx);  % Frame:  I.
-         Izz*rDot + p*q*r*(Iyy-Ixx)];
+HDot  = I*omegaDot; % Frame: I.
 
 % STEP 3: KINETICS - FORCES AND TORQUES.
 Fg    = [0; 0; -M*g]; % Frame: I.
-Ft    = [T; 0; 0];    % Frame: T.
+Ft    = [-T; 0; 0];    % Frame: T.
 taur  = [tauR; 0; 0]; % Frame: U
 
 % STEP 4: EULER'S FIRST LAW.
 % Note: Yields ux, uy, uz translational acceleration equations in V.
-firstLawLHS = Tt2u*Ft + Ti2u*Fg;
-firstLawRHS = Ti2u*M*a;
+firstLawLHS = Tu2i*Tt2u*Ft + Fg;
+firstLawRHS = M*a;
+firstLaw    = firstLawLHS == firstLawRHS;
 
 xEqnLHS = firstLawLHS(1);
 xEqnRHS = firstLawRHS(1);
@@ -123,8 +110,9 @@ zEqn    = zEqnLHS == zEqnRHS;
 
 % STEP 5: EULER'S SECOND LAW.
 % Note: Yields ux, uy, and uz rotational acceleration equations in V.
-secondLawLHS = Tt2u*cross(rho2, Ft) + taur;
+secondLawLHS = Tu2i*Tt2u*cross(rho2, Ft) + Tu2i*taur;
 secondLawRHS = HDot;
+secondLaw    = secondLawLHS == secondLawRHS;
 
 pEqnLHS = secondLawLHS(1);
 pEqnRHS = secondLawRHS(1);
@@ -137,35 +125,6 @@ qEqn    = qEqnLHS == qEqnRHS;
 rEqnLHS = secondLawLHS(3);
 rEqnRHS = secondLawRHS(3);
 rEqn    = rEqnLHS == rEqnRHS;
-
-% Euler's 1st law; in x, y, z axis order.
-%eqn1LHS = T*cos(xi)*cos(zeta) + M*g*sin(theta);
-%eqn1RHS = xDDot*cos(theta)*cos(psi) + yDDot*cos(theta)*sin(psi) - zDDot*sin(theta);
-%eqn1    = eqn1LHS == eqn1RHS;
-%
-%eqn2LHS = -T*sin(xi) - M*g*sin(phi)*sin(theta);
-%eqn2RHS = xDDot*(cos(phi)*sin(psi)+sin(phi)*sin(theta)*cos(psi)) ...
-%    + yDDot*(cos(phi)*cos(psi)+sin(phi)*sin(theta)*sin(psi)) + zDDot*sin(phi)*cos(theta);
-%eqn2    = eqn2LHS == eqn2RHS;
-%
-%eqn3LHS = T*cos(xi)*sin(zeta) - M*g*cos(phi)*cos(theta);
-%eqn3RHS = xDDot*(sin(phi)*sin(psi)+cos(phi)*sin(theta)*cos(psi)) ...
-%    + yDDot*(-sin(phi)*cos(psi)+cos(phi)*sin(theta)*sin(psi)) + zDDot*cos(phi)*cos(theta);
-%eqn3    = eqn3LHS == eqn3RHS;
-%
-%
-% Euler's 2nd law; in p, q, r order rotation order.
-%eqn4LHS = tauR;  % pretty stooopid o_O
-%eqn4RHS = Ixx*pDot + p*q*r*(Izz-Iyy);
-%eqn4    = eqn4LHS == eqn4RHS;
-%
-%eqn5LHS = rho2*T*cos(xi)*sin(zeta);
-%eqn5RHS = Iyy*qDot + p*q*r*(Izz-Ixx);
-%eqn5    = eqn5LHS == eqn5RHS;
-%
-%eqn6LHS = rho2*T*sin(xi);
-%eqn6RHS = Izz*rDot + p*q*r*(Iyy-Ixx);
-%eqn6    = eqn6LHS == eqn6RHS;
 
 
 % Solve equations of motion.
@@ -184,7 +143,7 @@ O = 2; % Order of Taylor series. Note that O > 2 is nonlinear!
  xDot0 = 0;   yDot0 = 0;   zDot0 =  0;
 xDDot0 = 0;  yDDot0 = 0;  zDDot0 = 0;
 
-theta0 = 0;  thetaDot0 = 0;  thetaDDot0 = 0;
+theta0 = pi/2;  thetaDot0 = 0;  thetaDDot0 = 0;
   psi0 = 0;    psiDot0 = 0;    psiDDot0 = 0;
   phi0 = 0;    phiDot0 = 0;    phiDDot0 = 0;
   xi0  = 0; 
@@ -192,30 +151,25 @@ theta0 = 0;  thetaDot0 = 0;  thetaDDot0 = 0;
 
 %syms T0 tauR0 % Useful for seeing where there show up in linearized solutions. 
               % Comment out for a specific solutions.
-T0 = 1; tauR0 = 0;
+T0 = 1; tauR0 = 1;
 
-xTaylorLHS = taylor(xEqnLHS, [T, xi, zeta, theta], [T0, xi0, zeta0, theta0], Order=O);
-xTaylorRHS = taylor(xEqnRHS, [xDDot, theta, psi, yDDot], ...
-    [xDDot0, theta0, psi0, yDDot0], Order=O);
-yTaylorLHS = taylor(yEqnLHS, [T, xi, phi, theta], [T0, xi0, phi0, theta0], Order=O);
-yTaylorRHS = taylor(yEqnRHS, [xDDot, yDDot, zDDot, phi, psi, theta], ...
-    [xDDot0, yDDot0, zDDot0, phi0, psi0, theta0], Order=O);
-zTaylorLHS = taylor(zEqnLHS, [T, xi, zeta, phi, theta], ...
-    [T0, xi0, zeta0, phi0, theta0], Order=O);
-zTaylorRHS = taylor(zEqnRHS, [xDDot, yDDot, zDDot, phi, psi, theta], ...
-    [xDDot0, yDDot0, zDDot0, phi0, psi0, theta0], Order=O);
-pTaylorLHS = taylor(pEqnLHS, tauR, tauR0, Order=O);
-pTaylorRHS = taylor(pEqnRHS, [phiDDot, psiDDot, theta, phi, psi, thetaDot, ...
-    phiDot, psiDot], [phiDDot0, psiDDot0, theta0, phi0, psi0, thetaDot0, ...
-    phiDot0, psiDot0], Order=O);
-qTaylorLHS = taylor(qEqnLHS, [T, xi, zeta], [T0, xi0, zeta0], Order=O);
-qTaylorRHS = taylor(qEqnRHS, [thetaDDot, psiDDot, phi, psi, theta, phiDot, ...
-    psiDot, thetaDot], [thetaDDot0, psiDDot0, phi0, psi0, theta0, ...
-    phiDot0, psiDot0, thetaDot0], Order=O);
-rTaylorLHS = taylor(rEqnLHS, [T, xi], [T0, xi0], Order=O);
-rTaylorRHS = taylor(rEqnRHS, [thetaDDot, psiDDot, phi, psi, theta, phiDot, ...
-    psiDot, thetaDot], [thetaDDot0, psiDDot0, phi0, psi0, theta0, ...
-    phiDot0, psiDot0, thetaDot0], Order=O);
+eqnvars = [x y z xDot yDot zDot xDDot yDDot zDDot theta thetaDot thetaDDot ...
+    psi psiDot psiDDot phi phiDot phiDDot T xi zeta tauR];
+eqninit = [x0 y0 z0 xDot0 yDot0 zDot0 xDDot0 yDDot0 zDDot0 theta0 ...
+    thetaDot0 thetaDDot0 psi0 psiDot0 psiDDot0 phi0 phiDot0 phiDDot0 T0 xi0 zeta0 tauR0];
+
+xTaylorLHS = taylor(xEqnLHS, eqnvars, eqninit, Order=O);
+xTaylorRHS = taylor(xEqnRHS, eqnvars, eqninit, Order=O);
+yTaylorLHS = taylor(yEqnLHS, eqnvars, eqninit, Order=O);
+yTaylorRHS = taylor(yEqnRHS, eqnvars, eqninit, Order=O);
+zTaylorLHS = taylor(zEqnLHS, eqnvars, eqninit, Order=O);
+zTaylorRHS = taylor(zEqnRHS, eqnvars, eqninit, Order=O);
+pTaylorLHS = taylor(pEqnLHS, eqnvars, eqninit, Order=O);
+pTaylorRHS = taylor(pEqnRHS, eqnvars, eqninit, Order=O);
+qTaylorLHS = taylor(qEqnLHS, eqnvars, eqninit, Order=O);
+qTaylorRHS = taylor(qEqnRHS, eqnvars, eqninit, Order=O);
+rTaylorLHS = taylor(rEqnLHS, eqnvars, eqninit, Order=O);
+rTaylorRHS = taylor(rEqnRHS, eqnvars, eqninit, Order=O);
 
 
 % Solve linearized equations of motion.
@@ -226,5 +180,5 @@ pTaylor = pTaylorLHS == pTaylorRHS;
 qTaylor = qTaylorLHS == qTaylorRHS;
 rTaylor = rTaylorLHS == rTaylorRHS;
 
-linSol = solve([xTaylor, yTaylor, zTaylor, pTaylor, qTaylor, rTaylor], ...
+linSol = solve([xTaylor, yTaylor, zTaylor, pTaylor, qTaylor, rTaylor], ...  
     [xDDot, yDDot, zDDot, thetaDDot psiDDot phiDDot]) % Linear solution.
